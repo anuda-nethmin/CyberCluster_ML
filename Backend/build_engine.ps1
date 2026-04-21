d351590a8b90d92860d7f09f47e95522e624d737
Anuda Nethmin <anudanethmin@hotmail.com>
2026-04-21 19:06:06 +0100

    Script to compile .c files to .o and them link them all together in to a final .exe with the math lib

---
+++ Backend/build_engine.ps1
@@ -0,0 +1,26 @@
+if (!(Test-Path "build")) { New-Item -ItemType Directory -Path "build" -Force | Out-Null }
+
+# Message to host
+Write-Host "Compiling C Backend..." -ForegroundColor Cyan
+
+# Compile all .c files to .o objects
+
+gcc -Wall -Wextra -O3 -c main.c -o build/main.o
+gcc -Wall -Wextra -O3 -c csv_parser.c -o build/csv_parser.o
+gcc -Wall -Wextra -O3 -c linear_regression.c -o build/linear_regression.o
+gcc -Wall -Wextra -O3 -c logistic_regression.c -o build/logistic_regression.o
+gcc -Wall -Wextra -O3 -c knn.c -o build/knn.o
+
+
+if ($?) {
+    # Link them all together into the final executable with the math library (-lm)
+    gcc -Wall -Wextra -O3 -o build/ml_engine.exe build/main.o build/csv_parser.o build/linear_regression.o build/logistic_regression.o build/knn.o -lm
+    
+    if ($?) {
+        Write-Host "Build Complete! Output saved to build/ml_engine.exe" -ForegroundColor Green
+    } else {
+        Write-Host "Linking failed." -ForegroundColor Red
+    }
+} else {
+    Write-Host "Compilation failed." -ForegroundColor Red
+}
