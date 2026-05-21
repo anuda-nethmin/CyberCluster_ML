5551a4d8f01220ee5d955aa98a537e8099a9440a
anuda-nethmin <anudanethmin@hotmail.com>
2026-05-21 13:59:14 +0100

    Users can customize vite port

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -21,4 +21,8 @@ import react from '@vitejs/plugin-react' // Official React plugin for Vite
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],  // Enable React JSX + Fast Refresh support
+  server: {
+    port: 3000,        // Change this to whatever port you want!
+    strictPort: true,  // (Optional) Forces Vite to fail if port 3000 is already in use
+  }
 })
